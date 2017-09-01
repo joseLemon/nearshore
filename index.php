060a0bfab047ed2521fdd490247949ea3901ccb8
José Angel Lujan Villaseñor <jose.lujan@lemonthree.mx>
2017-08-31 22:22:19 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include('header.php') ?>
+    <!-- ================================== -->
+
+    <!-- ///////////  BANNER  \\\\\\\\\\\ -->
+
+    <!-- ================================== -->
+    <div class="banner" id="banner">
+        <div class="container">
+            <div class="row no-margin vertical-align-abs">
+                <div class="col-sm-6 text-right">
+                    <h1 class="white">Neashore</h1>
+                </div>
+                <div class="col-sm-6">
+                    <p class="white">
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit,
+                        eiusmod tempor incididunt.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ================================== -->
+
+    <!-- ///////////  WHY US?  \\\\\\\\\\\ -->
+
+    <!-- ================================== -->
+    <div class="why-us site-section white-bg light-spacing" id="why-us">
+        <div class="container">
+            <h1 class="heading black">
+                WHY US?
+            </h1>
+            <h2 class="jumbo-shadow">
+                why us?
+            </h2>
+        </div>
+        <div class="row no-margin text-center">
+            <div class="col-sm-4">
+                <h3>Multilingual</h3>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Cusomized development</h3>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Specialized equipment</h3>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+            </div>
+        </div>
+    </div>
+<?php include('footer.php') ?>
