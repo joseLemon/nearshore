9d6db26e12c6e51c3be0526c6d555cc5649c5148
José Angel Lujan Villaseñor <jose.lujan@lemonthree.mx>
2017-09-01 22:08:28 -0600

    up until getting to services section

--- index.php
+++ index.php
@@ -5,65 +5,126 @@
 
     <!-- ================================== -->
     <div class="banner" id="banner">
-        <div class="container">
-            <div class="row no-margin vertical-align-abs">
+        <div class="container vertical-align-abs">
+            <div class="row no-margin eq-height-cols-table">
                 <div class="col-sm-6 text-right">
                     <h1 class="white">Nearshore</h1>
                 </div>
                 <div class="col-sm-6">
-                    <p class="white">
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit,
-                        eiusmod tempor incididunt.
-                    </p>
+                    <div class="right-side-text">
+                        <p class="white">
+                            Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit,
+                            eiusmod tempor incididunt.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- ================================== -->
+    <div class="content">
+        <!-- ================================== -->
 
-    <!-- ///////////  WHY US?  \\\\\\\\\\\ -->
+        <!-- ///////////  WHY US?  \\\\\\\\\\\ -->
 
-    <!-- ================================== -->
-    <div class="why-us site-section white-bg light-spacing" id="why-us">
-        <div class="container">
-            <h1 class="heading black">
-                WHY US?
-            </h1>
-            <h2 class="jumbo-shadow">
-                why us?
-            </h2>
+        <!-- ================================== -->
+        <div class="why-us site-section white-bg light-spacing" id="why-us">
+            <div class="container">
+                <h1 class="heading black">
+                    WHY US?
+                </h1>
+                <h2 class="jumbo-shadow">
+                    why us?
+                </h2>
+            </div>
+            <div class="row no-margin text-center eq-height-cols">
+                <div class="col-sm-4">
+                    <h3>Multilingual</h3>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <img src="img/decoration/1.png" alt="text bubble">
+                </div>
+                <div class="col-sm-4">
+                    <h3>Cusomized development</h3>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <img src="img/decoration/2.png" alt="pencil">
+                </div>
+                <div class="col-sm-4">
+                    <h3>Specialized equipment</h3>
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                    <img src="img/decoration/3.png" alt="cogs">
+                </div>
+            </div>
         </div>
-        <div class="row no-margin text-center">
-            <div class="col-sm-4">
-                <h3>Multilingual</h3>
-                <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <p>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <img src="img/decoration/1.png" alt="text bubble">
+        <!-- ================================== -->
+
+        <!-- ///////////  WHY IN MEXICO?  \\\\\\\\\\\ -->
+
+        <!-- ================================== -->
+        <div class="why-mex site-section black-bg" id="why-mex">
+            <div class="row no-margin eq-height-cols-table">
+                <div class="col-sm-5">
+                    <h1 class="heading white">
+                        WHY<br>
+                        IN<br>
+                        MEXICO?
+                        <span class="green-line"></span>
+                    </h1>
+                    <h2 class="jumbo-shadow white">
+                        Mexico
+                    </h2>
+                </div>
+                <div class="col-sm-7">
+                    <div class="img-bg">
+                        <div class="box-container spacing">
+                            <div class="white-box text-center">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum."
+                                <br>
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad minim veniam, quis nostrud exercitation.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-4">
-                <h3>Cusomized development</h3>
-                <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <p>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <img src="img/decoration/2.png" alt="pencil">
-            </div>
-            <div class="col-sm-4">
-                <h3>Specialized equipment</h3>
-                <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <p>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                </p>
-                <img src="img/decoration/3.png" alt="cogs">
+        </div>
+        <!-- ================================== -->
+
+        <!-- ///////////  SERVICES  \\\\\\\\\\\ -->
+
+        <!-- ================================== -->
+        <div class="services site-section white-bg" id="services">
+            <div class="row no-margin eq-height-cols-table">
+                <div class="col-sm-5">
+                    <h1>SERVICES</h1>
+                    <h2 class="jumbo-shadow">
+                        Services
+                    </h2>
+                </div>
+                <div class="col-sm-7">
+
+                </div>
             </div>
         </div>
     </div>
